7aa72dadd20ed155c78923de57cc04e3abd02e97
Priest476 <liamp0903@gmail.com>
2025-03-29 14:47:24 -0400

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ STATIC_FOLDER = "static"
 if not os.path.exists(STATIC_FOLDER):
     os.makedirs(STATIC_FOLDER)
 
-@app.route("/compare", methods=["GET"])
+@app.route("/compare", methods=["POST"])
 def home():
     """Serve the homepage with the form."""
     return render_template("index.html")
